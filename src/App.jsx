721f3e38243b34268da3459f16fafbf2b627ddc4
Error4qu <madhav4412@gmail.com>
2024-11-07 00:23:10 +0530

    complete password generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,69 @@
-import { useState } from 'react'
+import { useCallback, useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [length, setlength] = useState(8);
+  const [num, isnumber] = useState(false);
+  const [chr, ischar] = useState(false);
+  const [inpu, setinpu] = useState("");
+  const passgen = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
+    if (num) str += "123456789";
+    if (chr) str += "!@#$%^&*()<>?:";
 
-  return (
-    
-  )
+    for (let i = 0; i < length; i++) {
+      const pos = Math.floor(Math.random() * str.length);
+      pass += str[pos];
+    }
+    setinpu(pass);
+  }, [length, num, chr,]);
+useEffect(() => {
+  passgen();
+}, [length, num,chr,passgen])
+
+return (
+  <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-auto text-orange-500 bg-gray-700'>
+    <div className='flex shadow rounded-lg overflow-hidden mb-0 py-4'>
+      <input
+        type="text"
+        value={inpu}
+        className='outline-none w-full py-1 px-3 rounded-md'
+        placeholder='password'
+        readOnly
+      />
+      <button
+        //onClick={handleCopy}
+        className='bg-blue-700 px-3 mx-2 rounded-md outline-none py-0.5'>
+        Copy
+      </button>
+    </div>
+    <div className='flex gap-2 shadow rounded-lg overflow-hidden py-3'>
+      <div className='flex gap-2'>
+        <input
+          type="range"
+          className='cursor-pointer'
+          value={length}
+          min={6}
+          max={100}
+          onChange={(e) => setlength(Number(e.target.value))}
+          />
+        <label>Length ({length})</label>
+      </div>
+      <div className='flex gap-1' onClick={() => isnumber(!num)}>
+        <input type="checkbox" checked={num} onChange={() => {}} />
+        <p>Numbers</p>
+      </div>
+      <div className='flex gap-1' onClick={() => ischar(!chr)}>
+        <input type="checkbox" checked={chr} onChange={() => {}} />
+        <p>Characters</p>
+      </div>
+    </div>
+  </div>
+);
 }
 
 export default App
